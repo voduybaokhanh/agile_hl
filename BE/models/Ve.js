9f4083c780625ceffe2437564a534bba75f360ad
Phong908 <122242560+khanhvo908@users.noreply.github.com>
2024-11-21 20:02:56 +0700

    Revert "thêm bảng erd mới rạp , phòng chiếu, thanh toán"
    This reverts commit c96a9e9d0e023b9da26266c55aebfb9cf4424b69.

--- BE/models/Ve.js
+++ BE/models/Ve.js
@@ -2,15 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const ve = new Schema(
-  {
-    id: { type: ObjectId },
-    gheNgoi: { type: String, required: true, trim: true },
-    giaVe: { type: Number, required: true, min: 0 },
-    suatChieu: { type: ObjectId, ref: "suatChieu", required: true },
-    user: { type: ObjectId, ref: "user", required: true },
-  },
-  { timestamps: true } // Thêm createdAt và updatedAt
-);
+const ve = new Schema({
+  id: { type: ObjectId },
+  gheNgoi: { type: String, required: true },
+  giaVe: { type: Number, required: true },
+  suatChieu: { type: ObjectId, ref: "suatChieu", required: true },
+  gioHang: { type: ObjectId, ref: "gioHang", required: true },
+});
 
 module.exports = mongoose.models.ve || mongoose.model("ve", ve);
