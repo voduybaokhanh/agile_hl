28e7b22889218ffd2482bca0cddfa6dd3d222e7e
Phong908 <122242560+khanhvo908@users.noreply.github.com>
2024-11-27 13:11:01 +0700

    check

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Login from './login/login';
+
 function App() {
   return (
     <Router>
       <nav>
-        <Link to="/">Danh sách người dùng</Link>
-        <Link to="/add">Thêm người dùng</Link>
+      
       </nav>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/add" element={<AddUser />} />
+     
       </Routes>
     </Router>
   );
